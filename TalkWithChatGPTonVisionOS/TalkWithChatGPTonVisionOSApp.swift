32a1476a4d5305869e773b7f7b985c66394e344b
cranoo <75974473+cranoo3@users.noreply.github.com>
2024-02-08 00:02:23 +0900

    Initial Commit

---
+++ TalkWithChatGPTonVisionOS/TalkWithChatGPTonVisionOSApp.swift
@@ -0,0 +1,21 @@
+//
+//  TalkWithChatGPTonVisionOSApp.swift
+//  TalkWithChatGPTonVisionOS
+//
+//  Created by cranoo on 2024/02/08.
+//
+
+import SwiftUI
+
+@main
+struct TalkWithChatGPTonVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }.immersionStyle(selection: .constant(.progressive), in: .progressive)
+    }
+}
